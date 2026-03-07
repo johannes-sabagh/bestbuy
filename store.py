16a127be71702aa65a194b6d7dfb343a58c08abf
johannes-sabagh <johannes.sabagh@gmail.com>
2026-03-07 10:13:45 +0100

    store.py ready with basics

--- store.py
+++ store.py
@@ -1,7 +1,11 @@
 import products
 class Store:
-    def __init__(self):
-        self.products = []
+    def __init__(self, products=None):
+        if products is not None:
+            self.products = products
+        else:
+            self.products = []
+
     def add_product(self, product):
         self.products.append(product)
 
@@ -15,20 +19,30 @@ class Store:
         return total
 
     def get_all_products(self):
+        active_products = []
         for product in self.products:
-            if product.quantity > 0:
-                print(product)
-            else:
-                continue
+            if product.is_active():
+                active_products.append(product)
+        return active_products
 
     def order(self, shopping_list):
+        total_price = 0
+        for product, quantity in shopping_list:
+            if not product.is_active():
+                raise Exception(f"{product.name} is not available.")
+            if quantity > product.get_quantity():
+                raise Exception(f"Not enough stock for {product.name}.")
+            total_price += product.buy(quantity)
+        return total_price
 
 
 
+product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                products.Product("Google Pixel 7", price=500, quantity=250),
+               ]
 
-bose = products.Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-mac = products.Product("MacBook Air M2", price=1450, quantity=100)
-store = Store()
-store.add_product(bose)
-store.add_product(mac)
-print(store.products)
+best_buy = Store(product_list)
+products = best_buy.get_all_products()
+print(best_buy.get_total_quantity())
+print(best_buy.order([(products[0], 1), (products[1], 2)]))
